13fa6d061ada0aa19ca84337f6c8063ef8cfc989
zhepama <zhepama@gmail.com>
2024-05-13 11:58:36 +0800

    feat: Add Warp Tunnel endpoint sets for IPv4 and IPv6
    This commit adds two new files, `ipv4.txt` and `ipv6.txt`, which contain sets of IP addresses for Warp Tunnel endpoints. These files are used to configure the Warp Tunnel endpoint settings.
    
    The `ipv4.txt` file includes the following IP address ranges:
    - 162.159.192.0/24
    - 162.159.193.0/24
    - 162.159.195.0/24
    - 162.159.204.0/24
    - 188.114.96.0/24
    - 188.114.97.0/24
    - 188.114.98.0/24
    - 188.114.99.0/24
    
    The `ipv6.txt` file includes the following IP address ranges:
    - 2606:4700:d0::/48
    - 2606:4700:d1::/48
    
    These files are necessary for configuring the Warp Tunnel endpoint sets and improving the performance and reliability of the application.

---
+++ scoop_init.ps1
@@ -0,0 +1,42 @@
+﻿# 获取当前用户环境变量的"Path"
+$currentPath = [Environment]::GetEnvironmentVariable('Path', 'User')
+
+# 检查是否包含指定路径
+$targetPath = 'D:\Applications\Scoop\shims'
+$containsTargetPath = $currentPath -split ';' -contains $targetPath
+
+if (-not $containsTargetPath) {
+    # 如果不包含指定路径，则添加
+    $newPath = $currentPath + ';' + $targetPath
+    [Environment]::SetEnvironmentVariable('Path', $newPath, 'User')
+    Write-Host "已成功添加路径: $targetPath"
+
+    # 刷新环境变量（仅适用于当前会话）
+    $env:Path = [Environment]::GetEnvironmentVariable('Path', 'User')
+
+} else {
+    Write-Host "路径已存在: $targetPath"
+}
+
+# 打印出所有的"Path"
+$newPathList = [Environment]::GetEnvironmentVariable('Path', 'User') -split ';'
+Write-Host "所有的Path:"
+$newPathList
+
+
+# 设置SCOOP的ROOT PATH
+scoop config root_path D:\Applications\Scoop
+scoop config proxy 127.0.0.1:1080
+# 打印所有配置
+scoop config
+
+# 添加git仓库都为 安全
+git config --global --add safe.directory "*"
+
+## 建立run的插件目录
+
+# SymbolicLink是通用的文件和目录链接机制，可以链接目录和文件，且可以链接不同盘上的目录和文件。 就是快捷方式
+# Hard Link是在文件系统级别创建的链接，只能链接文件，且只能链接同一个盘上的文件。
+# Junction是Windows特有的目录连接机制，只能链接目录，且可以跨越不同的盘。
+New-Item -ItemType Junction -Path "$env:LOCALAPPDATA\Microsoft\PowerToys\PowerToys Run\Plugins" -Target "D:\Applications\Scoop\persist\PowerToys\PowerToys Run\Plugins"
+
---
+++ warp-tunnel-endpoint-set/warp-tunnel-endpoint-set.ps1
@@ -0,0 +1,67 @@
+# 执行 CloudflareWarpSpeedTest 并输出到 result.csv
+
+
+function Get-CurrentWarpEndpoint {
+    # 执行warp-cli settings命令并捕获输出
+    $output = warp-cli settings
+
+    # 使用正则表达式查找包含特定文本的行
+    $endpointLine = $output | Select-String "\(user set\)\s+Override WARP endpoint: (.+)"
+
+    # 如果找到行，则从匹配结果中提取IP:端口字符串
+    if ($endpointLine -ne $null) {
+        # 正则匹配结果中Groups集合的第二个元素包含了IP:端口字符串
+        $currentEndpoint = $endpointLine.Matches.Groups[1].Value
+
+        # 返回当前Endpoint
+        $currentEndpoint
+    } else {
+        return $null
+    }
+}
+
+Write-Output "运行前,确保你的所有代理都关闭了!"
+Read-Host "按 Enter 键继续"
+
+CloudflareWarpSpeedTest -f ipv4.txt -o result.csv
+
+# 等待命令执行完毕，确保结果已写入文件
+Start-Sleep -Seconds 2
+
+# 读取并解析 result.csv 文件
+$resultData = Get-Content -Path result.csv
+
+# 获取第二行记录（跳过标题行）
+$secondLine = $resultData[1]
+
+# 分析第二行内容并提取 IP 地址
+$endpoint = $secondLine.Split(',')[0]
+$loss = $secondLine.Split(',')[1]
+$delay = $secondLine.Split(',')[2]
+
+# 获得当前的endpoint
+$currentEndpoint = Get-CurrentWarpEndpoint
+if ($currentEndpoint) {
+    Write-Output "当前Endpoint: $currentEndpoint"
+}
+# 以下用 warp-cli 执行命令更新 endpoint
+warp-cli disconnect > $null 2>&1
+warp-cli tunnel endpoint reset > $null 2>&1
+warp-cli tunnel endpoint set $endpoint
+$currentEndpoint = Get-CurrentWarpEndpoint
+if ($currentEndpoint) {
+    Write-Output "当前Endpoint: $currentEndpoint"
+}
+warp-cli connect > $null 2>&1
+
+# 打印信息，即 result.csv 的第二行记录
+
+$message = @"
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::                                                                 ::
+           自选服务器已经设置为$endpoint
+           丢包率 $loss 平均延迟 $delay               
+::                                                                 ::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+"@
+Write-Output $message 
