b3a450cb2910d86ac6245062a4909553ad2e331b
zhepama <zhepama@gmail.com>
2024-05-15 14:54:34 +0800

    feat: Add cloudflarest/txt to .gitignore

--- cloudflarest/cloudflarest.ps1
+++ cloudflarest/cloudflarest.ps1
@@ -15,21 +15,21 @@ foreach ($port in $ports) {
     Write-Output "Testing port $port..."
 
     # `echo ""` 会发送一个空的字符串作为输入（类似用户只是按了 Enter）
-    echo "" | CloudflareST.exe -n 1000 -tp $port -dn 10  -p 10  -f "$PSScriptRoot\ipv4.txt" -o "$PSScriptRoot\result_$port.csv" -url $cloudflareSTUrl
+    echo "" | CloudflareST.exe -n 1000 -tp $port -dn 10  -p 10  -f "$PSScriptRoot\ipv4.txt" -o "$PSScriptRoot\result-$port.csv" -url $cloudflareSTUrl
     
     # 等待命令执行完毕，确保结果已写入文件
     Start-Sleep -Seconds 2
     
     # 读取并解析对应端口号的 result.csv 文件
-    $csvContent = Get-Content -Path "$PSScriptRoot\result_$port.csv"
+    $csvContent = Get-Content -Path "$PSScriptRoot\result-$port.csv"
     
     # 跳过标题行，并遍历CSV中的每一行
     $csvContent | Select-Object -Skip 1 | ForEach-Object {
         # 切割每行以逗号为分隔符
         $columns = $_ -split ','
 
-        # 检查条件 延迟 必须小于 1000 且 下载速度 必须大于 1，否则跳过这行
-        if ($columns[4] -ge 1000 -or $columns[5] -le 1) {
+        # 检查条件丢包率大于75% 或者 延迟大于1000 或者 下载速度小于 1，跳过这行
+        if ($columns[3] -ge 0.75 -or $columns[4] -ge 1000 -or $columns[5] -le 1) {
             continue
         }
 
